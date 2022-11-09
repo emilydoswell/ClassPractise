3935fe015eec5374a7f74f7f483fcc84a0d22dc6
emilydoswell <70842968+emilydoswell@users.noreply.github.com>
2022-11-09 13:28:30 +0000

    finishing touches

--- Documents/Wiley/ClassPractise/MyLayeredEmployeeCollectionProject/src/main/java/emily/service/EmployeeService.java
+++ Documents/Wiley/ClassPractise/MyLayeredEmployeeCollectionProject/src/main/java/emily/service/EmployeeService.java
@@ -1,8 +1,10 @@
 package emily.service;
 
 import emily.entity.Employee;
+import emily.entity.EmployeePaySlip;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface EmployeeService {
     Collection<Employee> getAllEmployees();
@@ -14,4 +16,8 @@ public interface EmployeeService {
     boolean deleteEmployee(int id);
 
     boolean incrementSalary(int id,double increment);
+
+    EmployeePaySlip generatePaySlip(int employeeId);
+
+    List<Employee> searchEmployeeByDepartment(String empDepartment);
 }
--- Documents/Wiley/ClassPractise/MyLayeredEmployeeCollectionProject/src/main/java/emily/service/EmployeeServiceImpl.java
+++ Documents/Wiley/ClassPractise/MyLayeredEmployeeCollectionProject/src/main/java/emily/service/EmployeeServiceImpl.java
@@ -1,10 +1,13 @@
 package emily.service;
 
 import emily.entity.Employee;
+import emily.entity.EmployeePaySlip;
 import emily.persistence.EmployeeDao;
 import emily.persistence.EmployeeDaoImpl;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class EmployeeServiceImpl implements EmployeeService {
     //Service will request persistence layer for all data needs
@@ -17,6 +20,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee searchEmployeeById(int id) {
+
         return employeeDao.searchRecord(id);
     }
 
@@ -39,6 +43,17 @@ public class EmployeeServiceImpl implements EmployeeService {
         return false;
     }
 
+    @Override
+    public boolean incrementSalary(int id, double increment) {
+        Employee employee=employeeDao.searchRecord(id);
+        if(employee==null) {
+            return false;
+        }
+        employee.setEmpSalary(employee.getEmpSalary()+increment);
+        employeeDao.insertRecord(employee);
+        return true;
+    }
+
     /*
      * Business Rule
      * AllowancesA : 18% of salary
@@ -47,8 +62,36 @@ public class EmployeeServiceImpl implements EmployeeService {
      */
 
     @Override
-    public boolean incrementSalary(int id, double increment) {
+    public EmployeePaySlip generatePaySlip(int employeeId) {
+        Employee employee=employeeDao.searchRecord(employeeId);
+        if(employee!=null) {
+            double allowanceA=.18*employee.getEmpSalary();
+            double allowanceB=.12*employee.getEmpSalary();
+            double deduction=.08*employee.getEmpSalary();
+            double totalSalary=employee.getEmpSalary()+allowanceA+allowanceB-deduction;
 
-        return false;
+            EmployeePaySlip employeePaySlip=new EmployeePaySlip(employee, allowanceA, allowanceB, deduction,totalSalary);
+            return employeePaySlip;
+        }
+        return null;
+
+    }
+
+    @Override
+    public List<Employee> searchEmployeeByDepartment(String empDepartment) {
+        Collection<Employee> emp = employeeDao.getAllRecords();
+        ArrayList<Employee> employeeArrayList = new ArrayList<Employee>();
+
+        for (Employee employee : emp ) {
+            if (employee.getEmpDepartment().equals(empDepartment)) {
+                employeeArrayList.add(employee);
+            }
+        }
+
+        if (employeeArrayList.isEmpty()) {
+            return null;
+        }
+
+        return employeeArrayList;
     }
 }
